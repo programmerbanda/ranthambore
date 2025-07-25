a9731c287364243f924279ceb0f3e10185388a9f
Tiwari Shubham <Shubham.injoria@gmail.com>
2025-07-25 22:00:11 +0530

    connect both page

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+const contactBtn = document.getElementById('contact-btn');
+contactBtn.addEventListener('click', ()=> {
+    window.location.href = 'contact.html';
+});
+
+const logo = document.querySelector('.logo');
+logo.addEventListener('click', ()=> {
+    window.location.href = 'index.html';
+});
