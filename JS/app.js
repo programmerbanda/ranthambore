c62e983a95b352465d7dbd2138ba22cc03941a5c
Tiwari Shubham <Shubham.injoria@gmail.com>
2025-08-04 11:26:29 +0530

    update

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,3 @@
-const contactBtn = document.getElementById('contact-btn');
-contactBtn.addEventListener('click', ()=> {
-    window.location.href = 'contact.html';
-});
-
 const logo = document.querySelector('.logo');
 logo.addEventListener('click', ()=> {
     window.location.href = 'index.html';
