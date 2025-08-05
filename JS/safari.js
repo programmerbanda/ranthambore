b76fff76252f7cffc4122fc7fb51a7000b3d168a
Tiwari Shubham <Shubham.injoria@gmail.com>
2025-08-05 11:03:01 +0530

    add safari.js

---
+++ JS/safari.js
@@ -0,0 +1,12 @@
+const safariBookingBtn = document.getElementById('safari-booking-btn');
+const userFullName = document.getElementById('name');
+const userEmail = document.getElementById('email');
+const userNumber = document.getElementById('mobile');
+const userTimng = document.getElementById('timing');
+const safari = document.getElementById('safari');
+const zone = document.getElementById('zone');
+
+console.log(zone)
+safariBookingBtn.addEventListener('click', ()=> {
+    console.log('Hello')
+})
