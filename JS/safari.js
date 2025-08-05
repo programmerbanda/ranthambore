2ffaedc8b90abe184260932a9522da5230cd26fa
Tiwari Shubham <Shubham.injoria@gmail.com>
2025-08-05 12:12:01 +0530

    add

--- JS/safari.js
+++ JS/safari.js
@@ -1,12 +1,25 @@
-const safariBookingBtn = document.getElementById('safari-booking-btn');
-const userFullName = document.getElementById('name');
-const userEmail = document.getElementById('email');
-const userNumber = document.getElementById('mobile');
-const userTimng = document.getElementById('timing');
-const safari = document.getElementById('safari');
-const zone = document.getElementById('zone');
+const form = document.getElementById('form')
+const safariBookingBtn = document.getElementById("safari-booking-btn");
+const userFullName = document.getElementById("name").value;
+const userEmail = document.getElementById("email").value;
+const userNumber = document.getElementById("mobile").value;
+const userTimng = document.getElementById("timing").value;
+const safari = document.getElementById("safari").value;
+const zone = document.getElementById("zone").value;
+const safariTiming = document.querySelector('.safari-timing');
 
-console.log(zone)
-safariBookingBtn.addEventListener('click', ()=> {
-    console.log('Hello')
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    localStorage.setItem('username', userFullName)
+    localStorage.setItem('email', userEmail)
+    localStorage.setItem('number', userNumber)
+    localStorage.setItem('timing', userTimng)
+    localStorage.setItem('safari', safari)
+    localStorage.setItem('zone', zone)
+    window.location.href = 'create-safari-booking.html'
 })
+
+// safariBookingBtn.addEventListener("click", () => {
+//     console.log('submit clicked')
+//   window.location.href = "create-safari-booking.html";
+// });
